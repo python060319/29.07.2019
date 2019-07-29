8a85cac34d5c9aa51a7bd6f2ff63efdf720a1d2e
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-29 20:15:45 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,8 +4,9 @@ class Car:
         print("Creating a car")
         self.__secret()
     # method
-    def drive(self):
+    def drive(self, *args):
         print("driving a car...")
+        print(args)
     def washCar(self):
         print("washing my car...")
     def __secret(self):
@@ -14,8 +15,8 @@ class Car:
 toyota = Car()
 mitsubishi = Car()
 print(type(toyota))
-toyota.drive()
-toyota.__secret()
+toyota.drive(3, 4)
+#toyota.__secret()
 
 
 # function
@@ -30,3 +31,9 @@ def foo():
 #   powerOff
 # create secret method: factoryRestart
 # create 2 phones
+
+# Class Calculator
+# __init__ - print creating
+# Sum
+# def Sum(self, *args):
+#   print sum of args
