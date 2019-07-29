0fc7d5a69cfe6fd494cf4fa82c7d563a717fb721
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-29 18:17:21 +0300

    Update args.py

--- args.py
+++ args.py
@@ -3,15 +3,19 @@ def foo(*args):
     print(args)
     print(type(args))
     print(args[0])
-    args[0] = 12
+    #args[0] = 12 # Error!!
 
 foo(1,2,3,'a', [1,2,3])
 
-mySum(1, 2, 3)
+mySum(1, 2, 2, 3, 3)
 # print sum of numbers
-# return list of numbers - given by *args
-# modify *args to *myargs - did it work?
-# return the given args without duplications
+# return list of numbers -
+#   given by *args
+#   [1, 2, 2, 3, 3]
+# modify *args to *myargs -
+#   did it work?
+# return the given args
+#   without duplications
 foo( )
 
 
