6f8278d73905740c5d84ef28f49505b58fb8a6a8
python060319 <51337287+python060319@users.noreply.github.com>
2019-07-29 20:20:34 +0300

    Update classes.py

--- classes.py
+++ classes.py
@@ -16,7 +16,7 @@ toyota = Car()
 mitsubishi = Car()
 print(type(toyota))
 toyota.drive(3, 4)
-#toyota.__secret()
+#toyota.__secret() - Error!
 
 
 # function
@@ -32,8 +32,33 @@ def foo():
 # create secret method: factoryRestart
 # create 2 phones
 
+class MobilePhone:
+    def __init__(self):
+        print("Creating a phone...")
+        self.__factoryRestart()
+    def ring(self):
+        print("phone is ringing")
+    def powerOn(self):
+        print("phone is ringing")
+    def powerOff(self):
+        print("phone is ringing")
+    def __factoryRestart(self):
+        print("factory restart")
+
+iphoneX = MobilePhone()
+iphoneX.ring()
+
 # Class Calculator
 # __init__ - print creating
 # Sum
 # def Sum(self, *args):
 #   print sum of args
+
+class Calculator:
+    def __init__(self):
+        print("Creating calculator")
+    def sum(self, *args):
+        print(sum(args))
+
+c = Calculator()
+c.sum(1,2,3,4,5)
